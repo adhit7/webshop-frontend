f7a7596dad0d99155e1f3f2870bfe56bceb0b546
adhit7 <adhithyanalan@gmail.com>
2024-08-04 20:46:49 +0530

    axios url changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './bootstrap.min.css'
-import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import store from './store'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://web-production-0d9f9.up.railway.app';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
