b5426d89aacc9450159c12955c05ce658cc1fc65
adhit7 <adhithyanalan@gmail.com>
2024-08-16 10:17:01 +0530

    minor changes

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart, removeFromCart } from '../actions/cartActions';
 import Message from '../components/Message';
@@ -14,20 +14,20 @@ import {
 } from 'react-bootstrap';
 
 const CartScreen = ({ match, location, history }) => {
-  // const productId = match.params.id;
+  const productId = match.params.id;
 
-  // const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
 
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
-  // useEffect(() => {
-  //   if (productId) {
-  //     dispatch(addToCart(productId, qty));
-  //   }
-  // }, [dispatch, productId, qty]);
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
     dispatch(removeFromCart(id));
