83dc228af3c53c0c46d3d982f3a5b8c6441dbdb7
adhit7 <adhithyanalan@gmail.com>
2024-12-11 17:23:07 +0530

    backend url change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://web-production-0d9f9.up.railway.app';
+axios.defaults.baseURL = 'https://webshop-2wqm.onrender.com';
 
 ReactDOM.render(
   <Provider store={store}>
